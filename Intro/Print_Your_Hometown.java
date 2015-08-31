cdc3f2ef5321065ae2babd9adec76eeb183ae739
Galya <alice.gospodinova@gmail.com>
2015-08-31 16:54:58 +0300

    Intro Java programs

---
+++ Intro/Print_Your_Hometown.java
@@ -0,0 +1,6 @@
+package Intro;
+public class Print_Your_Hometown {
+    public static void main(String[] args) {
+        System.out.println("Hello I am from Javaland!");
+    }
+}
---
+++ Intro/problem5__Print_CurrentDate.java
@@ -0,0 +1,9 @@
+package Intro;
+
+import java.util.Date;
+
+public class problem5__Print_CurrentDate {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
