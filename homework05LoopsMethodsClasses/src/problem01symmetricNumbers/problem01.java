098dcbd94ef87f18beed919fa98d78e44ac41d4b
Galya <alice.gospodinova@gmail.com>
2015-09-04 09:36:40 +0300

    Find  symmetric numbers program.

--- homework05LoopsMethodsClasses/src/problem01symmetricNumbers/problem01.java
+++ homework05LoopsMethodsClasses/src/problem01symmetricNumbers/problem01.java
@@ -1,6 +1,8 @@
 package problem01symmetricNumbers;
 import org.apache.commons.lang.StringUtils;
-
+/*
+Write a program to generate and print all symmetric numbers in given range [start…end] (0 ? start ? end ? 999). A number is symmetric if its digits are symmetric toward its middle. For example, the numbers 101, 33, 989 and 5 are symmetric, but 102, 34 and 997 are not symmetric.
+ */
 import java.util.Scanner;
 
 public class problem01 {
