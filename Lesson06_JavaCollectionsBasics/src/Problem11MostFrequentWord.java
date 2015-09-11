97ee388dc0b0874d5b3f751f845c67edb40a2bf7
Galya <alice.gospodinova@gmail.com>
2015-09-11 15:17:16 +0300

    Program extracts all words from text and print them in alphabetical order, no repeating words.

---
+++ Lesson06_JavaCollectionsBasics/src/Problem11MostFrequentWord.java
@@ -0,0 +1,18 @@
+import java.util.*;
+/*
+At the first line at the console you are given a piece of text. Extract all words from it and print them in alphabetical order. Consider each non-letter character as word separator. Take the repeating words only once. Ignore the character casing. Print the result words in a single line, separated by spaces.
+ */
+public class Problem11MostFrequentWord {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine().toLowerCase();
+
+        List<String> words = new ArrayList<>(Arrays.asList(text.split("\\W")));
+        Set<String> orderedWords = new TreeSet<>(words);
+
+        System.out.println(String.join(" ", orderedWords));
+
+    }
+}
+
+
