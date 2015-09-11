666d27b845af33587fa355079bd5012f3d16e75a
Galya <alice.gospodinova@gmail.com>
2015-09-11 15:21:23 +0300

    fix file name

--- Lesson06_JavaCollectionsBasics/src/Problem10ExtractAllUniqueWords.java
+++ Lesson06_JavaCollectionsBasics/src/Problem10ExtractAllUniqueWords.java
@@ -2,7 +2,7 @@ import java.util.*;
 /*
 At the first line at the console you are given a piece of text. Extract all words from it and print them in alphabetical order. Consider each non-letter character as word separator. Take the repeating words only once. Ignore the character casing. Print the result words in a single line, separated by spaces.
  */
-public class Problem11MostFrequentWord {
+public class Problem10ExtractAllUniqueWords {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String text = scan.nextLine().toLowerCase();
